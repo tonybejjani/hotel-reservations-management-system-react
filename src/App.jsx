8f8676595f33298ed82b207d89cea6c59f53a08a
Tony <bejjani.tony@gmail.com>
2024-12-11 13:33:52 +1100

    1- implement GlobalStyles to App.jsx 2- structure UI components Button and Input into seperate files 3- Apply hover effect to UI Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 /** @format */
 
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 2.4rem;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Hello world!</H1>
+        <Button onClick={() => alert('clicked!')}>Check in</Button>
+        <Button>Check out</Button>
+        <Input placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
