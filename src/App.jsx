1768e0b9714b80a4dacb73e0da2d2c163cfd007d
Tony <bejjani.tony@gmail.com>
2024-12-12 19:10:08 +1100

    Design and Build <AppLayou /> : 1- add a sidebar 2- add a header 3- add <Outlet /> develop a css grid to fit all layout components in the <AppLayout />

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,24 @@ import Settings from './pages/Settings';
 import Cabins from './pages/Cabins';
 import Users from './pages/Users';
 import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
 function App() {
   return (
     <>
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="account" element={<Account />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="login" element={<Login />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
-          <Route path="*" element={<PageNotFound />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="login" element={<Login />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
