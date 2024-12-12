760873888358c0dcb6ca39800c0307499eb86a0a
Tony <bejjani.tony@gmail.com>
2024-12-12 15:53:40 +1100

    Develop App routes: 1- deploy react-router-dom 2- connect the routes to the pages 3- import pages All prev steps exist in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,32 @@
 /** @format */
 
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/heading';
-import Row from './ui/Row';
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
-
+import Dashboard from './pages/Dashboard';
+import Account from './pages/Account';
+import Bookings from './pages/Bookings';
+import Login from './pages/Login';
+import Settings from './pages/Settings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import PageNotFound from './pages/PageNotFound';
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Escape</Heading>
-            <div>
-              <Heading as="h2">Check In & out</Heading>
-              <Button onClick={() => alert('clicked!')}>Check in</Button>
-              <Button size="small" variation="primary">
-                Check out
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <form>
-              <Heading as="h3">Form</Heading>
-              <Input placeholder="Number of guests" />
-              <Input placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,5 +1,7 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+/** @format */
+
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Bookings() {
   return (
