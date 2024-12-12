7f9993bcd3cb0b048bc5ca03753e6c729e24c133
Tony <bejjani.tony@gmail.com>
2024-12-12 12:54:27 +1100

    1- Enhance  Button.jsx styled component: include reusable props 2- Build Row.jsx styled component: include reusable props

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Heading from './ui/heading';
+import Row from './ui/Row';
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -16,14 +16,31 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">Escape</Heading>
-
-        <Heading as="h2">Check In & out</Heading>
-        <Button onClick={() => alert('clicked!')}>Check in</Button>
-        <Button>Check out</Button>
-
-        <Heading as="h3">Form</Heading>
-        <Input placeholder="Number of guests" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Escape</Heading>
+            <div>
+              <Heading as="h2">Check In & out</Heading>
+              <Button
+                size="small"
+                variation="primary"
+                onClick={() => alert('clicked!')}
+              >
+                Check in
+              </Button>
+              <Button size="small" variation="primary">
+                Check out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <form>
+              <Heading as="h3">Form</Heading>
+              <Input placeholder="Number of guests" />
+              <Input placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
