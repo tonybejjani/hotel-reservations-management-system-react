ce1b817326aa027c3e03efb3b37c63464659bd31
Tony <bejjani.tony@gmail.com>
2025-03-22 12:14:06 +1100

    Build a reusable Compound Component Table for the CabinTable

--- src/ui/Table-v1.jsx
+++ src/ui/Table-v1.jsx
@@ -1,6 +1,5 @@
 /** @format */
 
-import { createContext, useContext } from 'react';
 import styled from 'styled-components';
 
 export const StyledTable = styled.div`
@@ -61,42 +60,3 @@ export const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
-
-const TableContext = createContext();
-
-function Table({ children, columns }) {
-  return (
-    <TableContext.Provider value={{ columns }}>
-      <StyledTable role="table">{children}</StyledTable>
-    </TableContext.Provider>
-  );
-}
-
-function Header({ children }) {
-  const { columns } = useContext(TableContext);
-  return (
-    <StyledHeader role="row" columns={columns} as="header">
-      {children}
-    </StyledHeader>
-  );
-}
-
-function Row({ children }) {
-  const { columns } = useContext(TableContext);
-  return (
-    <StyledRow role="row" columns={columns}>
-      {children}
-    </StyledRow>
-  );
-}
-
-function Body({ children }) {
-  return { children };
-}
-
-Table.Header = Header;
-Table.Row = Row;
-Table.Body = Body;
-Table.Footer = Footer;
-
-export default Table;
