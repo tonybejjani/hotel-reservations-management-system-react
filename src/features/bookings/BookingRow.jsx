ea7d7698f5ae80faf8b5856c67919437c6a81f97
Tony <bejjani.tony@gmail.com>
2025-04-08 09:45:47 +1000

    Develop the Booking "check in"  html in the context menu. Add the onClick event navigation to it. Render it only on "unconfirmed" booking status; Meaning customers can only be checked in if they have not done so already.

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -9,7 +9,7 @@ import Table from '../../ui/Table';
 import { formatCurrency } from '../../utils/helpers';
 import { formatDistanceFromNow } from '../../utils/helpers';
 import Menus from '../../ui/Menus';
-import { HiEye } from 'react-icons/hi2';
+import { HiArrowDownOnSquare, HiEye } from 'react-icons/hi2';
 import { useNavigate } from 'react-router-dom';
 
 const Cabin = styled.div`
@@ -95,8 +95,16 @@ function BookingRow({
           >
             See details
           </Menus.Button>
-          <Menus.Button>Check in</Menus.Button>
-          <Menus.Button>Delete booking</Menus.Button>
+
+          {status === 'unconfirmed' && (
+            <Menus.Button
+              icon={<HiArrowDownOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check in
+            </Menus.Button>
+          )}
+          {/* <Menus.Button>Delete booking</Menus.Button> */}
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
