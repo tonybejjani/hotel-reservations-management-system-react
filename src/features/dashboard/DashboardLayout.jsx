b294abcd67b25437eed8b52794220b897751ad05
Tony <bejjani.tony@gmail.com>
2025-04-15 11:01:56 +1000

    Render the pie chart from react re-chart and customise it. Integrate confirmedStays data to it and mold the data according to the chart api

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import useRecentStays from './useRecentStays';
 import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -29,6 +30,7 @@ function DashboardLayout() {
   return (
     <StyledDashboardLayout>
       <Stats bookings={bookings} staysConfirmed={staysConfirmed} />
+      <DurationChart staysConfirmed={staysConfirmed} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
